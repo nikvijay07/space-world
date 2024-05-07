12307577a234924c452b873be24a8de1d21a4761
nikvijay07 <nikvijay07@gmail.com>
2024-05-07 00:20:19 -0400

    adding rocket and space background

--- personal-website/src/App.jsx
+++ personal-website/src/App.jsx
@@ -1,16 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, Suspense } from 'react'
 import './App.css'
+import { Canvas } from '@react-three/fiber';
+import RocketPage from './components/RocketPage'
+
 
 function App() {
 
   return (
     <>
-      <Canvas>
-        <Rocket>
-        </Rocket>
-      </Canvas>
+      <RocketPage />
     </>
   )
 }
--- personal-website/vite.config.js
+++ personal-website/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  // resolve: {
+  //   alias: {
+  //     'three': 'three/build/three.module.js'
+  //   }
+  // },
   plugins: [react()],
-})
+});
