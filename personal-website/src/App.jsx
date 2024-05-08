7efeb4583ab2ae965aec1a342c3b0d75d6444d9f
nikvijay07 <nikvijay07@gmail.com>
2024-05-07 22:08:54 -0400

    smooth movement

--- personal-website/src/App.jsx
+++ personal-website/src/App.jsx
@@ -1,30 +1,28 @@
-import { useState, useRef, Suspense } from 'react'
+import { useEffect, useRef, Suspense } from 'react'
 import './App.css'
-import { Canvas } from '@react-three/fiber';
-import StarTest from './components/StarTest';
+import { Canvas, render } from '@react-three/fiber';
 import Background from './components/Background';
-import { OrbitControls, PerspectiveCamera } from '@react-three/drei';
+import { OrbitControls, PerspectiveCamera, PresentationControls } from '@react-three/drei';
 import Rocket from '../public/Rocket'
 const App = () => {
-  // Ref for accessing the OrbitControls instance
   const controls = useRef();
 
-  // Function to log camera position and orientation
-  const logCameraInfo = () => {
-    console.log('Camera Position:', controls.current.object.position);
-    console.log('Camera Rotation (Euler):', controls.current.object.rotation);
-  };
+  // Function to update camera position and orientation to follow the rocket
+  // Call updateCamera whenever the rocket position changes;
+
 
   return (
-    <Canvas onClick={logCameraInfo}>
+    <Suspense fallback={null}>
+    <Canvas>
+      {/* <PresentationControls snap global zoom={0.1} rotation={[0, -Math.PI / 2, 0]} polar={[0, Math.PI / 4]} azimuth={[-Math.PI / 4, Math.PI / 8]}> */}
       <PerspectiveCamera makeDefault fov={20} position={[38, -46, 0]} />
       <OrbitControls ref={controls} />
       <ambientLight />
-      <Suspense fallback={null}>
         <Background />
-        <Rocket />
-      </Suspense>
+        <Rocket/>
+      {/* </PresentationControls> */}
     </Canvas>
+    </Suspense>
   );
 };
 
