1d019475478394d6fc818fd68af5a58f1a88c55a
nikvijay07 <nikvijay07@gmail.com>
2024-07-11 14:49:53 -0400

    config changes

--- personal-website/public/Rocket.jsx
+++ personal-website/public/Rocket.jsx
@@ -1,7 +1,6 @@
 import React, { useRef, useEffect, forwardRef } from 'react'
 import { useGLTF } from '@react-three/drei'
 import * as THREE from 'three';
-import { useFrame } from '@react-three/fiber'
 import { useRocket } from '../src/hooks/useRocket';
 
 
