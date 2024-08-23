11203ea74b791fc5722ff8e8071eaea84b32adfd
nikvijay07 <nikvijay07@gmail.com>
2024-08-23 15:51:14 -0400

    speed change

--- personal-website/public/Rocket.jsx
+++ personal-website/public/Rocket.jsx
@@ -11,7 +11,7 @@ const Rocket = forwardRef((props, ref) => {
 
 
   let isArrowUp = false;
-  let stepSize = 0.22; // Adjust as needed
+  let stepSize = 0.08; // Adjust as needed
   let isArrowDown = false;
 
   const animate = () => {
